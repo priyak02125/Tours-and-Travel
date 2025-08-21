3d5099bfa89b4708da3b8f7ab3855c57c630d497
NoorAzam-1 <azamnoor698@gmail.com>
2025-08-21 13:03:34 +0530

    header footer set in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
+import Header from "@/components/Header";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Tour And Travel",
@@ -8,7 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
